59a7f0b166d1e642b5a7b0a23d302f6cd60566ca
tqtan <48000201+tqtan@users.noreply.github.com>
2019-07-18 15:04:27 +0800

    Update maxProduct.java

--- maxProduct.java
+++ maxProduct.java
@@ -1,1 +1,15 @@
-
+    public int maxProduct(int[] nums) {
+        int pro = Integer.MIN_VALUE;
+        int c_max = 1,c_min = 1;
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] < 0){
+                int temp = c_max;
+                c_max = c_min;
+                c_min = temp;
+            }
+            c_max = Math.max(c_max * nums[i],nums[i]);
+            c_min = Math.min(c_min * nums[i],nums[i]);
+            pro = Math.max(c_max,pro);
+        }
+        return pro;
+    }
