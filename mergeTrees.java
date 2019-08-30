176fb3ed84919fcc4ac2bb4b4d808b2ba3628572
tqtan <48000201+tqtan@users.noreply.github.com>
2019-08-30 09:26:06 +0800

    树-617.合并二叉树

---
+++ mergeTrees.java
@@ -0,0 +1,11 @@
+    # 树结构用递归时，只能返回可以参加运算的，不能参加运算的不能以0的形式返回
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return null;
+        if (t1 == null) return t2;
+        if (t2 == null) return t1;
+        TreeNode root = new TreeNode(t1.val + t2.val);
+        root.left = mergeTrees(t1.left,t2.left);
+        root.right = mergeTrees(t1.right,t2.right);
+        return root;
+        
+    }
